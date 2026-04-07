d4f25ea72ed0b038b3ec7d1b818acdc0c156e831
eastmanb13 <eastmanb13@gmail.com>
2026-04-07 14:32:08 -0700

    initial commit

---
+++ scripts/myscripts.R
@@ -0,0 +1,8 @@
+getwd()
+View(iris)
+
+#configure to git by running this in the terminal
+#git config --global user.name "eastmanb13"
+#git config --global user.email "eastmanb13@gmail.com"
+#git config --global init.defaultBranch main
+#git config --global --list
